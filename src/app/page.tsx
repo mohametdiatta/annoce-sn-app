79629bcc50296a8a732dff055189a8fd9a880c22
mohamet <mohamed.diatta@paps-app.com>
2023-09-29 09:23:08 +0000

    mcard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,10 @@ import AnnonceItem from "@/components/anonce-item";
 
 export default function Home() {
   return (
-    <div>
-      <AnnonceItem />
+    <div className="mt-2 grid grid-cols-3 gap-3">
+      {Array.from({ length: 8 }).map((item, i) => (
+        <AnnonceItem key={i} />
+      ))}
     </div>
   );
 }
--- src/components/anonce-item.tsx
+++ src/components/anonce-item.tsx
@@ -15,32 +15,29 @@ import { Label } from "@/components/ui/label";
 
 function AnnonceItem() {
   return (
-    <Card className="w-[350px]">
+    <Card className="w-[250px]">
       <CardHeader>
         <CardTitle>
           <Image
             src={
               "https://images.unsplash.com/photo-1695754190419-bedddd21ccb3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyM3x8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
             }
-            width={320}
+            width={220}
             alt=""
-            className="rounded-sm h-[130px]"
+            className="rounded-sm h-[110px]"
             height={20}
           />
         </CardTitle>
-        <CardDescription>
-          Les candidats sont de plus en plus en quête de sens au travail.
-          Au-delà de l’emploi en lui-même et des missions proposées.
-        </CardDescription>
+        <CardDescription>Les candidats sont de plus en p.</CardDescription>
       </CardHeader>
-      <CardContent>
+      {/* <CardContent>
         <div className="grid w-full items-center gap-4">
           <div className="flex flex-col space-y-1.5">
             <Label htmlFor="name">+221783071757</Label>
           </div>
         </div>
-      </CardContent>
-      <CardFooter className="flex justify-between">
+      </CardContent> */}
+      <CardFooter className="flex flex-row-reverse">
         <Button>Voir plus </Button>
       </CardFooter>
     </Card>
